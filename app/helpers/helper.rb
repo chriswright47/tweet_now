2bae78565079a62bb85d8f44c6573e6752fdff24
Chris Wright <christopher.wright47@gmail.com>
2013-11-12 16:59:29 -0600

    trying to get heroku to work

--- app/helpers/helper.rb
+++ app/helpers/helper.rb
@@ -2,7 +2,7 @@ helpers do
 
   def create_consumer
     OAuth::Consumer.new(
-      'CONSUMER_KEY', 'CONSUMER_SECRET', :site => 'https://api.twitter.com')
+      ENV['CONSUMER_KEY'], ENV['CONSUMER_SECRET'], :site => 'https://api.twitter.com')
   end
 
   def request_token
